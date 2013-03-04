d46fbf13becd2ef3b8125e39c0a1c699f85df509
Jowin de Bondt <jowin.de.bondt@gmail.com>
2013-03-04 14:51:34 +0100

    Changed dungeonfactory sprite

--- Mario/Mario/DungeonThemeFactory.cpp
+++ Mario/Mario/DungeonThemeFactory.cpp
@@ -3,15 +3,15 @@
 #include "DungeonThemePipe.h"
 
 HANDLE DungeonThemeFactory::getBlock(int x, int y){
-	return LoadImage(NULL, "res/dungeon.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+	return LoadImage(NULL, "res/world8.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
 }
 
 HANDLE DungeonThemeFactory::getPipe(int x, int y){
-	return LoadImage(NULL, "res/dungeon.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+	return LoadImage(NULL, "res/world8.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
 }
 
 HANDLE DungeonThemeFactory::getGround(int x, int y){
-	return LoadImage(NULL, "res/dungeon.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+	return LoadImage(NULL, "res/world8.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
 }
 
 HANDLE DungeonThemeFactory::getBackgroundImage(){
