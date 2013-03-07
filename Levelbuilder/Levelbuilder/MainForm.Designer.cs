1a1b67e292249a5cd22012630db599f9cdb8c17c
Jowin de Bondt <jowin.de.bondt@gmail.com>
2013-03-07 09:18:53 +0100

    First upset

---
+++ Levelbuilder/Levelbuilder/MainForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Levelbuilder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.gb_Tiles = new System.Windows.Forms.GroupBox();
+            this.comboBox_Theme = new System.Windows.Forms.ComboBox();
+            this.label_Theme = new System.Windows.Forms.Label();
+            this.panel = new System.Windows.Forms.Panel();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.gameobjectBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.gb_Tiles.SuspendLayout();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gameobjectBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gb_Tiles
+            // 
+            this.gb_Tiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.gb_Tiles.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.gb_Tiles.Controls.Add(this.comboBox_Theme);
+            this.gb_Tiles.Controls.Add(this.label_Theme);
+            this.gb_Tiles.Location = new System.Drawing.Point(13, 13);
+            this.gb_Tiles.MaximumSize = new System.Drawing.Size(286, 704);
+            this.gb_Tiles.Name = "gb_Tiles";
+            this.gb_Tiles.Size = new System.Drawing.Size(286, 687);
+            this.gb_Tiles.TabIndex = 0;
+            this.gb_Tiles.TabStop = false;
+            this.gb_Tiles.Text = "Tiles";
+            // 
+            // comboBox_Theme
+            // 
+            this.comboBox_Theme.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox_Theme.FormattingEnabled = true;
+            this.comboBox_Theme.Items.AddRange(new object[] {
+            "Select theme...",
+            "Landscape",
+            "Dungeon",
+            "Sky",
+            "Water"});
+            this.comboBox_Theme.Location = new System.Drawing.Point(69, 22);
+            this.comboBox_Theme.Name = "comboBox_Theme";
+            this.comboBox_Theme.Size = new System.Drawing.Size(168, 24);
+            this.comboBox_Theme.TabIndex = 1;
+            this.comboBox_Theme.Text = "Select theme...";
+            this.comboBox_Theme.SelectedIndexChanged += new System.EventHandler(this.comboBox_Theme_SelectedIndexChanged);
+            // 
+            // label_Theme
+            // 
+            this.label_Theme.AutoSize = true;
+            this.label_Theme.Location = new System.Drawing.Point(7, 25);
+            this.label_Theme.Name = "label_Theme";
+            this.label_Theme.Size = new System.Drawing.Size(56, 17);
+            this.label_Theme.TabIndex = 0;
+            this.label_Theme.Text = "Theme:";
+            // 
+            // panel
+            // 
+            this.panel.BackgroundImage = global::Levelbuilder.Properties.Resources.backgroundSky;
+            this.panel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel.Controls.Add(this.dataGridView);
+            this.panel.Location = new System.Drawing.Point(306, 13);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1377, 730);
+            this.panel.TabIndex = 2;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToResizeColumns = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.ColumnHeadersHeight = 32;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView.ColumnHeadersVisible = false;
+            this.dataGridView.DataSource = this.gameobjectBindingSource;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.WhiteSmoke;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView.GridColor = System.Drawing.Color.Black;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.Margin = new System.Windows.Forms.Padding(0);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.RowHeadersWidth = 32;
+            this.dataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.Transparent;
+            this.dataGridView.RowTemplate.DefaultCellStyle.ForeColor = System.Drawing.Color.Black;
+            this.dataGridView.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.WhiteSmoke;
+            this.dataGridView.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.dataGridView.RowTemplate.Height = 32;
+            this.dataGridView.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView.Size = new System.Drawing.Size(1377, 730);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.Scroll += new System.Windows.Forms.ScrollEventHandler(this.dataGridView_Scroll);
+            // 
+            // gameobjectBindingSource
+            // 
+            this.gameobjectBindingSource.DataSource = typeof(Levelbuilder.Gameobject);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1306, 624);
+            this.Controls.Add(this.gb_Tiles);
+            this.Controls.Add(this.panel);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Levelbuilder for SuperMario";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.gb_Tiles.ResumeLayout(false);
+            this.gb_Tiles.PerformLayout();
+            this.panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gameobjectBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gb_Tiles;
+        private System.Windows.Forms.ComboBox comboBox_Theme;
+        private System.Windows.Forms.Label label_Theme;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.BindingSource gameobjectBindingSource;
+    }
+}
+
---
+++ Levelbuilder/Levelbuilder/MainForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Levelbuilder
+{
+    public partial class MainForm : Form
+    {
+        const int ROWS = 22;
+        const int COLUMNS = 43;
+        Gameobject[][] level;
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            level = new Gameobject[ROWS][];
+            for (int i = 0; i < ROWS; i++)
+            {
+                level[i] = new Gameobject[COLUMNS];
+            }
+
+            dataGridView.DataSource = level;
+            for (int i = 0; i < COLUMNS; i++)
+            {
+                DataGridViewImageColumn img = new DataGridViewImageColumn();
+                img.HeaderText = "Image";
+                img.Name = i.ToString();
+                img.ValuesAreIcons = false;
+
+                dataGridView.Columns.Add(img);
+            }
+
+            for (int i = 0; i < ROWS; i++)
+            {
+                for (int j = 0; j < COLUMNS; j++)
+                {
+                    dataGridView.Rows[i].Cells[j].Value = Properties.Resources.ground;
+                }
+            }
+        }
+
+        private void comboBox_Theme_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox_Theme.SelectedItem.ToString())
+            {
+                case "Landscape":
+                    //tableLayoutPanel.BackgroundImage = Properties.Resources.backgroundSky;
+                    break;
+
+                case "Dungeon":
+                    //tableLayoutPanel.BackgroundImage = Properties.Resources.backgroundCave;
+                    break;
+
+                case "Sky":
+
+                    break;
+
+                case "Water":
+
+                    break;
+            }
+        }
+
+        private void dataGridView_Scroll(object sender, ScrollEventArgs e)
+        {
+            dataGridView.Invalidate();
+        }
+    }
+}
